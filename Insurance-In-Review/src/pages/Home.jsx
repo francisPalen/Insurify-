a177db92934c77137259c2b96d43963be6c84b2b
marcvillareal <marcvillareal30@gmail.com>
2024-02-18 01:07:16 +0000

    Added abstract and mobile design on bottom carousel

--- Insurance-In-Review/src/pages/Home.jsx
+++ Insurance-In-Review/src/pages/Home.jsx
@@ -40,37 +40,47 @@ export default function Home() {
       </div>
       <div>
         <div // Bottom Carousel
-          className="hero min-h-screen"
+          className="hero"
           style={{
             backgroundImage: "url(/Circle ABSTRACT.png)",
             background:
               "linear-gradient(0deg, rgba(94, 23, 235, 0.9), #ffffff 40%)",
           }}
         >
-          <div className="hero-content flex-col lg:flex-row-reverse">
-            <img
-              src="/InsurifyMobileLight.png"
-              className="max-w-sm rounded-lg shadow-4xl w-60"
-            />
+          <div className="flex-col lg:flex-row-reverse mr-96">
             <div>
-              <h1 className="pr-32 text-5xl font-bold text-insurify-grey">
+              <h1 className="text-5xl font-bold text-insurify-grey">
                 One App, One Location, Everything you need
               </h1>
               <p className="mb-5 pt-10 text-insurify-grey text-xl text-opacity-50">
-                Mobilize Your Peace of Mind: Simplicity, Security, and Seamless{" "}
+                Mobilize Your Peace of Mind: Simplicity, Security, and Seamless
                 <br />
-                Safeguarding, All in One Snapshot! - Discover the app that will{" "}
+                Safeguarding, All in One Snapshot! - Discover the app that will
                 <br />
                 centralise all your needs and more.
               </p>
-              <button className="btn btn-md bg-insurify-purple text-white rounded-box ml-54 w-40">
+              <button className="btn btn-md bg-insurify-purple text-white rounded-box ml-54 w-40 relative">
                 get the app
               </button>
             </div>
           </div>
+          <div className="grid grid-cols-2">
+            <div>
+              <img
+                src="/MobileBottomCarousel.png"
+                className="shadow-4xl w-64 mt-20 absolute right-56"
+              />
+            </div>
+            <div className="">
+              <img
+                src="/Circle ABSTRACT.png"
+                className="ml-80 right-0 mt-8 mb-2"
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <img src="Footer Banner.png" />
+      <img src="Footer Banner.png" className="relative" />
     </div>
   );
 }
