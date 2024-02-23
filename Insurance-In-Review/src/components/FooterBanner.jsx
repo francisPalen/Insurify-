062c2bcfba23bfd6cb920c81c2118473f5ed0459
marcvillareal <marcvillareal30@gmail.com>
2024-02-23 23:28:08 +0000

    Minor changes to the footer banner

--- Insurance-In-Review/src/components/FooterBanner.jsx
+++ Insurance-In-Review/src/components/FooterBanner.jsx
@@ -1,8 +1,8 @@
 export default function FooterBanner() {
   return (
-    <header className="flex lg:flex-row-reverse justify-between bg-insurify-purple text-white shadow-md tablet:py-10 mobile:py-5">
-      <div className="font-inter font-bold tablet:text-4xl tablet:pr-20 mobile:pl-64 mobile:text-xl ">
-        <span className="text-insurify-footer-banner">secure</span> your life
+    <header className="flex lg:flex-row-reverse justify-between bg-insurify-purple text-white shadow-md tablet:py-10 mobile:py-5 ">
+      <div className="font-inter font-bold tablet:text-4xl tablet:pr-20 mobile:pl-52 mobile:text-xl md:pl-96 md:ml-20 xs:pl-64 sm:pl-96 mdlg:pl-96">
+        <span className="text-insurify-footer-banner ">secure</span> your life
         <span className="text-base">®</span>
       </div>
     </header>
