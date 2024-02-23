75e0275ec2028417112582d4ef5d574debe28dd7
Marc Vincent Villareal <mvillareal01@qub.ac.uk>
2024-02-23 23:07:11 +0000

    Revert "Merge branch 'marc/frontend' into 'main'"
    This reverts merge request !7

---
+++ Insurance-In-Review/tailwind.config.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "grid-pattern": "url('/Grid Background')",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      bubblegum: "#ff77e9",
+      bermuda: "#78dcca",
+      // Insurify colour scheme below
+      "insurify-purple": "#5E17EB",
+      "insurify-grey": "#2B4148",
+      "insurify-dark": "#1D1F22",
+      "insurify-button-active": "#DBDBDB",
+      "insurify-login-background": "#EDEDED",
+      "insurify-grey-2": "#848484",
+      "insurify-input": "#3A3A3A",
+      "insurify-footer-banner": "#B99AF7",
+    },
+    // Custom font below
+    fontFamily: {
+      body: ["Montserrat", "sans-serif"],
+      footer: ["Jomhuria", "san-serif"],
+      "insurify-roboto": ["roboto"],
+      "insurify-patua": ["patua"],
+      "insurify-inter": ["inter"],
+    },
+    container: {
+      center: true,
+      padding: "1.25rem",
+    },
+    screens: {
+      sm: "528px",
+      md: "768px",
+      mdlg:"627px",
+      lg: "1063px",
+      mobile: "300px",
+      tablet: "1064px",
+      laptop: "1210px",
+      desktop: "1280px",
+    },
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui"), require("@tailwindcss/forms")],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        black: {
+          ...require("daisyui/src/theming/themes")["black"],
+          primary: "black",
+          secondary: "#5E17EB",
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "0.5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem", // border radius of tabs
+        },
+      },
+    ],
+  },
+};
